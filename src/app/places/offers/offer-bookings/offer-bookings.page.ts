54ca42a5334c6ccb49d0ab9a5f22f4dc5d033af3
CarlesRa <jcramos8282@gmail.com>
2020-06-30 18:08:11 +0200

    exercici 3 pel meu conter

--- src/app/places/offers/offer-bookings/offer-bookings.page.ts
+++ src/app/places/offers/offer-bookings/offer-bookings.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { PlacesService } from '../../places.service';
 import { NavController } from '@ionic/angular';
+import { Place } from '../../place.model';
 
 @Component({
   selector: 'app-offer-bookings',
@@ -10,26 +11,21 @@ import { NavController } from '@ionic/angular';
 })
 export class OfferBookingsPage implements OnInit {
 
+  place: Place;
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private placesService: PlacesService,
-    private router: Router,
     private navCtrl: NavController
   ) { }
 
   ngOnInit() {
     this.activatedRoute.paramMap.subscribe(paramMap => {
       if (!paramMap.has('placeId')) {
-        this.router.navigateByUrl('places/tabs/offers');
+        this.navCtrl.navigateBack('places/tabs/offers');
         return;
       }
-      const offerId = paramMap.get('placeId');
+      this.place = this.placesService.getPlace(paramMap.get('placeId'));
     });
   }
-
-  onNewBook() {
-    this.navCtrl.navigateBack('places/tabs/offers');
-  }
-
 }
